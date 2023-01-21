cfb2056d46ac1f0154041887e7d58e8ddda79d3d
Rodolfo <grm.vera@yavirac.edu.ec>
2023-01-21 12:09:53 -0500

    fix: correcion de errores y igualacion hasta la clase 20-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-import { PageModule } from './pages/page.module';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
-import { NoFoundComponent } from './no-found/no-found/no-found.component';
-import { SharedComponent } from './shared/shared/shared.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavLeftComponent } from './shared/nav-left/nav-left.component';
+import { PagesComponent } from './pages/pages.component';
+import { NoFoundComponent } from './no-found/no-found.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesModule } from './pages/page.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NoFoundComponent,
-    SharedComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    HeaderComponent,
-    NavLeftComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    PageModule,
-    SharedModule,
-    AuthModule,
-    HttpClientModule,
+      AppComponent,
+      PagesComponent,
+      NoFoundComponent,
+      LoginComponent,
+      RegisterComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  imports: [
+      BrowserModule,
+      AppRoutingModule,
+      PagesModule,
+      SharedModule,
+      HttpClientModule,
+  ]
 })
 export class AppModule { }
