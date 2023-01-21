e48e4437079d5ad7dad88b89da529996b5e3ff8f
Rodolfo <grm.vera@yavirac.edu.ec>
2023-01-21 11:10:09 -0500

    feat: services y product.ts

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ProductModel } from 'src/app/models/product.models';
+import { ProductHttpService } from 'src/app/services/product-http.service';
 
 @Component({
   selector: 'app-product',
@@ -7,80 +8,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  products: ProductModel[] = [];
+  selectedProduct:UpdateProductDto = {};
 
-  constructor(
-    private httpClient: HttpClient,
-  ) { }
-
+  constructor(private productHttpService:ProductHttpService) {
+    this.initProduc();
+   }
 
   ngOnInit(): void {
-    this.createProduct();
-    // this.getProducts();
-    //this.getProductsId();
+    this.getProducts();
+    //this.getProduct();
+    //this.createProduct();
     //this.updateProduct();
-    this.deleteProduct();
+    //this.deleteProduct();
   }
 
-  private url: string = "https://api.escuelajs.co/api/v1/products";
-
-  data = {
-
-    title: 'Geovanny Vera',
-    price: 5,
-    description: 'Quinto - A',
-    images: [],
-    categoryId: 1,
+  initProduc(){
+    this.selectedProduct={title:'', price:0, description:''}
   }
 
-  getProducts() {
-
-    this.httpClient.get(this.url).subscribe(response => {
-
-      console.log(response);
-
-    })
-  };
-
-  getProductsId() {
-
-    this.httpClient.get(this.url).subscribe(response => {
-
-      console.log(response);
-
-    })
-  };
-
-  createProduct() {
-
-    this.httpClient.post(this.url, this.data).subscribe(
-      response => {
-
-        console.log('response');
-
-      }
-    );
-  }
-
-
-  updateProduct() {
-
-    this.httpClient.put(this.url, this.data).subscribe(
-      response => {
-
-        console.log('response');
-
-      }
-    );
-  }
-
-  deleteProduct() {
-    
-    this.httpClient.delete(this.url).subscribe(
-      response => {
-
+    getProducts() {
+      this.productHttpService.getAll().subscribe(
+        response => {
+          this.products = response;
+          console.log(response);
+        });
+    }
+    getProduct() {
+      this.productHttpService.getOne(2).subscribe(
+        response => {
         console.log(response);
+      });
+    }
 
-      }
-    );
+    createProduct() {
+      const data = {
+        title: 'Computadora Itel core i7',
+        price: 650,
+        description: 'Electrodomesticos / Erick Guevara',
+        images: [
+          'https://m.media-amazon.com/images/I/51A+xXT0yiL._AC_SY580_.jpg',
+        ],
+        categoryId: 1,
+      };
+      this.productHttpService.store(data).subscribe(
+        response => {
+        console.log(response);
+      });
+    }
+    editProduct(product: ProductModel){
+      this.selectedProduct = product;
+    }
+    updateProduct(id:ProductModel['id']) {
+      const data = {
+        title: 'Computadora Itel core i10',
+        price: 1150,
+        description: 'Electrodomesticos / Erick Guevara',
+      };
+      this.productHttpService.update(id, data).subscribe(
+        response => {
+        console.log(response);
+      });
+    }
+    deleteProduct(id: ProductModel['id']) {
+      this.productHttpService.destroy(id).subscribe(
+        response => {
+          this.products = this.products.filter(product => product.id != id );
+        console.log(response);
+      });
+    }
   }
-}
