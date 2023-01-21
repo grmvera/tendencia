76b885503b8603be243be16aadf4f1ecba6bbec9
Rodolfo <grm.vera@yavirac.edu.ec>
2023-01-21 11:44:34 -0500

    feat: creacion de no found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesRoutingModule } from './pages/page-routing.module';
 import { AuthRoutingModule } from './auth/auth-routing.module';
+import { NoFoundComponent } from './no-found/no-found.component';
 
 
 
 const routes:Routes = [
 
   {path:'', redirectTo:'/dashboard', pathMatch:'full'},
-  //{path:'**', component:NoFoundComponent}
+  {path:'**', component:NoFoundComponent}
 
 ]
 @NgModule({
